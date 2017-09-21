60a1063a9fe90e4b5847673c92e7fddfcf968311
Paolo Salvadori <paolosalvadori@mbp-di-paolo.homenet.telecomitalia.it>
2017-09-21 09:10:08 +0200

    piccole modifiche di pulizia a app.js

--- UNADUNA/WebContent/javascripts/app/app.js
+++ UNADUNA/WebContent/javascripts/app/app.js
@@ -1,12 +1,8 @@
 (function(){
-
 	angular.module('unadunaModule', ['ngMaterial'], function($mdThemingProvider) {
 	    $mdThemingProvider.theme('docs-dark', 'default')
 	      .primaryPalette('yellow')
 	      .dark();
-	    
-	    
 	    });
-	
 })();
 
